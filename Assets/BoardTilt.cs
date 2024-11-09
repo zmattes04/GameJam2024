4ceca82de78544a37477ab22f2636eeb35890c43
Zach Mattes <zachmattes@gmail.com>
2024-11-08 21:04:45 -0800

    Added Board and Tilt

---
+++ Assets/BoardTilt.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardTilt : MonoBehaviour
+{
+    public Vector3 rotation = new Vector3(0f, 100f, 0f);
+    private float rotX;
+    private float rotY;
+    
+    public float verticalRotationSpeed;
+    public float horizontalRotationSpeed;
+
+    private void Update()
+    {
+        rotX = Input.GetAxis("Mouse Y") * verticalRotationSpeed;
+        rotY = Input.GetAxis("Mouse X") * horizontalRotationSpeed;
+        Debug.Log(rotX);
+        
+        rotation.x = rotX;
+        
+        rotation.y = 0;
+        rotation.z = rotY;
+
+        transform.Rotate(rotation * Time.deltaTime);
+    }
+}
+
