83fb7face5ad061b5753bb7e72c7423198cee639
Zach Mattes <zachmattes@gmail.com>
2024-11-10 10:02:29 -0800

    Updated holes logic and coin spawn

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -1,5 +1,47 @@
+// using UnityEngine;
+// using TMPro;
+
+// public class Coin : MonoBehaviour
+// {
+//     public int score = 0;
+//     public float xMin = -10f;
+//     public float xMax = 10f;
+//     public float zMin = -10f;
+//     public float zMax = 10f;
+//     public TMP_Text scoreText;
+//     public TMP_Text highScoreText;
+//     public SoundEffectPlayer soundEffectPlayer;
+//     public SoundEffectType soundEffectType;
+
+//     void Start()
+//     {
+//         GameManager.UpdateHighScore(GameManager.highScore, highScoreText);
+//     }
+
+//     private void OnTriggerEnter(Collider other)
+//     {
+//         if (other.CompareTag("Ball"))
+//         {
+//             score++;
+//             if (score > GameManager.highScore)
+//             {
+//                 GameManager.UpdateHighScore(score, highScoreText);
+//             }
+//             Debug.Log("Score: " + score);
+
+//             float randomX = Random.Range(xMin, xMax);
+//             float randomZ = Random.Range(zMin, zMax);
+
+//             transform.position = new Vector3(randomX, transform.position.y, randomZ);
+//             scoreText.text = "Score: " + score;
+//             soundEffectPlayer.PlaySoundEffect(soundEffectType);
+//         }
+//     }
+// }
+
 using UnityEngine;
 using TMPro;
+using System.Collections.Generic;
 
 public class Coin : MonoBehaviour
 {
@@ -12,6 +54,8 @@ public class Coin : MonoBehaviour
     public TMP_Text highScoreText;
     public SoundEffectPlayer soundEffectPlayer;
     public SoundEffectType soundEffectType;
+    public GenerateHoles generateHolesScript;  // Reference to GenerateHoles script
+    public float minDistanceFromHole = 1.5f;   // Minimum safe distance from holes
 
     void Start()
     {
@@ -29,12 +73,37 @@ public class Coin : MonoBehaviour
             }
             Debug.Log("Score: " + score);
 
-            float randomX = Random.Range(xMin, xMax);
-            float randomZ = Random.Range(zMin, zMax);
+            Vector3 newPosition;
+            do
+            {
 
-            transform.position = new Vector3(randomX, transform.position.y, randomZ);
+                float randomX = Random.Range(xMin, xMax);
+                float randomZ = Random.Range(zMin, zMax);
+                newPosition = new Vector3(randomX, transform.position.y, randomZ);
+            } while (!IsSafePosition(newPosition));
+
+            transform.position = newPosition;
             scoreText.text = "Score: " + score;
             soundEffectPlayer.PlaySoundEffect(soundEffectType);
         }
+    }
+
+    // Check if the generated position is at a safe distance from all holes
+    private bool IsSafePosition(Vector3 position)
+    {
+        List<Vector3> holePositions = generateHolesScript.getHolePositions();
+        foreach (Vector3 holePosition in holePositions)
+        {
+            float distance = Vector3.Distance(position, holePosition);
+            if (distance < minDistanceFromHole)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public int getscore()
+    {
+        return score;
     }
 }
--- Assets/Scripts/GenerateHoles.cs
+++ Assets/Scripts/GenerateHoles.cs
@@ -1,22 +1,38 @@
 using UnityEngine;
 using Parabox.CSG;
+using System.Collections.Generic;
 
 public class GenerateHoles : MonoBehaviour
 {
     public float minX, maxX, minZ, maxZ;
-
+    public List<Vector3> holePositions = new List<Vector3>();
     public GameObject PerformSubtraction(GameObject targetObject, GameObject subtractMeshObject, Vector3 scale)
+{
+    Vector3 newPosition;
+    float xPos, zPos;
+    const float minDistanceFromBall = 1.0f;
+
+    do
     {
-        float xPos = Random.Range(minX, maxX);
-        float zPos = Random.Range(minZ, maxZ);
-        Vector3 newPosition = new Vector3(xPos, subtractMeshObject.transform.position.y, zPos);
-        subtractMeshObject.transform.position = newPosition;
-        Model result = CSG.Subtract(targetObject, subtractMeshObject);
-        targetObject.GetComponent<MeshFilter>().sharedMesh = result.mesh;
-        targetObject.GetComponent<MeshRenderer>().sharedMaterials = result.materials.ToArray();
-        targetObject.GetComponent<MeshCollider>().sharedMesh = null;
-        targetObject.GetComponent<MeshCollider>().sharedMesh = targetObject.GetComponent<MeshFilter>().sharedMesh;
-        targetObject.transform.localScale = scale;
-        return targetObject;
+        xPos = Random.Range(minX, maxX);
+        zPos = Random.Range(minZ, maxZ);
+        newPosition = new Vector3(xPos, subtractMeshObject.transform.position.y, zPos);
+    }
+    while (Vector3.Distance(newPosition, Vector3.zero) < minDistanceFromBall);
+
+    subtractMeshObject.transform.position = newPosition;
+    holePositions.Add(newPosition);
+
+    Model result = CSG.Subtract(targetObject, subtractMeshObject);
+    targetObject.GetComponent<MeshFilter>().sharedMesh = result.mesh;
+    targetObject.GetComponent<MeshRenderer>().sharedMaterials = result.materials.ToArray();
+    targetObject.GetComponent<MeshCollider>().sharedMesh = null;
+    targetObject.GetComponent<MeshCollider>().sharedMesh = targetObject.GetComponent<MeshFilter>().sharedMesh;
+    targetObject.transform.localScale = scale;
+    return targetObject;
+}
+    public List<Vector3> getHolePositions()
+    {
+        return holePositions;
     }
 }
